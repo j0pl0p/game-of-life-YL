53d837fda3297645266fd211bb62f15f69f442bc
Egor Lomakin <egorlomakin@MacBook-Pro-Egor.local>
2022-12-05 20:15:14 +0300

    get_cell created

--- Board.py
+++ Board.py
@@ -8,8 +8,13 @@ class Board:
         self.top = 10
         self.cell_size = 10
 
-    # настройка внешнего вида
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
+    def get_cell(self, pos):
+        '''pos: (width_pixel, height_pixel)
+        return None, None если клетка не в поле'''
+        width, height = pos
+        width -= self.left
+        height -= self.top
+        if (0 <= width <= self.width * self.cell_size
+                and 0 <= height <= self.height * self.cell_size):
+            return width // self.cell_size, height // self.cell_size
+        return None, None

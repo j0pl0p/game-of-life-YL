070fb8b07cbbff93b6e01c1e466bf616bacd164a
arrgn <cemas045@vk.com>
2022-12-05 20:14:42 +0300

    Fix project architecture errors

--- main.py
+++ main.py
@@ -18,3 +18,22 @@ class Board:
                 self.matrix[get_cell(pos)] = True
             elif event.button == 3:
                 self.matrix[get_cell(pos)] = False
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_caption("Main window")
+    size = width, height = 1500, 800
+    screen = pygame.display.set_mode(size)
+    running = True
+    fps = 100
+    clock = pygame.time.Clock()
+    b_color = (0, 0, 0)
+    while running:
+        screen.fill(b_color)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()

3fa33081c8f62a4cb32c1025bfb054c8f5279fb0
NumPix <lenchik221006@gmail.com>
2022-12-05 20:06:37 +0300

    Finished making mouse event functions

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame as pg
+import pygame.mouse
+from pygame.locals import *
+
+
+class Board:
+    def __init__(self):
+        self.matrix = [[bool, ...], ...]
+
+    def get_cell(self, coords: (int, int)) -> (int, int):
+        pass
+
+    def set_life(self, event: pg.event):
+        if event.type == MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+
+            if event.button == 1:
+                self.matrix[get_cell(pos)] = True
+            elif event.button == 3:
+                self.matrix[get_cell(pos)] = False
